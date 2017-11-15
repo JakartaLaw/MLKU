158851b8055b1ad26fc045b92df4786b30b4644c
JakartaLaw <jeppesjohansen@gmail.com>
2017-11-15 21:32:14 +0100

    worked with sorting in numpy

--- assignment1/modules.py
+++ assignment1/modules.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# %%
 
 class LoadData:
 
@@ -43,7 +43,7 @@ class LoadData:
 
         feature_names = ["f{}".format(i) for i in range(len(training_features.T))] #names for X_variable
         df_train = pd.DataFrame(data=training_features, columns=feature_names)
-        df_train['labels'] = training_labels
+        df_train['labs'] = training_labels
 
         #For test set
         test_features = np.genfromtxt("MNIST-cropped-txt//MNIST-Test-cropped.txt")
@@ -56,7 +56,7 @@ class LoadData:
 
         feature_names = ["f{}".format(i) for i in range(len(test_features.T))] #names for X_variable
         df_test = pd.DataFrame(data=test_features, columns=feature_names)
-        df_test['labels'] = test_labels
+        df_test['labs'] = test_labels
 
         return df_train, df_test
 
@@ -72,3 +72,44 @@ def ShowImage(df_digits,row):
     df_temp=df_digits.drop(labels="labels",axis=1)
     plt.imshow(np.array(df_temp.iloc[row:row+1]).reshape(28,28).T, cmap="Greys")
     plt.show()
+# %%
+
+
+df = pd.DataFrame({'a':[1,2,3,4],'b':[3,4,5,6],'c':[1,1,7,8]})
+
+df.head()
+
+test = np.array([1,2,3])
+
+ones = np.ones(df.shape[0])
+
+data = np.array(df)
+subtractor = np.outer(ones,test)
+
+y = data - subtractor
+
+
+y
+d_before_sq = np.diag(np.inner(y,y)) # distance before squareing
+d = np.sqrt(d_before_sq) # all distances
+d
+
+
+A = np.array([5, 2, 3, 4])
+B = np.array([9, 7, 8, 6])
+print("lars")
+C = np.array([A,B])
+
+print(C)
+C.T
+D = np.argsort(C[:1])
+print(D)
+C[:1]
+
+a = np.argsort(A)
+print(a)
+print(B)
+
+sorted = B[a]
+
+print(sorted)
